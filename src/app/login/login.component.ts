ae3fb5834f3c806b3a438178d713f4fb58f479dc
le.trung01 <40650762+letrung1998vn@users.noreply.github.com>
2024-11-12 14:11:33 +0700

    update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-
+import {FormControl,FormGroup} from '@angular/forms';
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  accountForm= new FormControl({
+    userName: new FormControl(''),
+    password: new FormControl('')
+  });
+  
 }
